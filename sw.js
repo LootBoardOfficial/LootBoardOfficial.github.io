817fbc7224ebb4214b54feef307e25d4ab72399e
LootBoardOfficial <mrbiswadeepsharma@gmail.com>
2026-07-26 00:48:09 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+// LootBoard App Engine (Service Worker)
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('lootboard-store').then((cache) => cache.addAll([
+      '/',
+      '/index.html'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
